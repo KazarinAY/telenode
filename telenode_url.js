d23ebf261a38857f21af625deb1d8baf72250a87
Alexandr <new_bazzz@mail.ru>
2015-06-23 10:54:16 +0400

    asdfw

--- telenode_url.js
+++ telenode_url.js
@@ -2,7 +2,7 @@ var http = require("http");
 var url = require("url");
 var fs = require('fs');
 var exec = require("child_process").exec;
-var child = initTelegram(true);
+var child = initTelegram(false);
 
 var server = http.createServer(function(req, res) {
         var pathname = url.parse(req.url).pathname;
@@ -26,10 +26,7 @@ function initTelegram(isStdoutOn) {
     if (isStdoutOn) {
         child.stdout.on('data', function(data) {
             if (data == "> ") {return;}             
-            console.log("DATA \"" + data + "\"");  
-            fs.appendFile('log.txt', data, function (err) {
-                if (err) return console.log(err);                
-            }); 
+            console.log("DATA \"" + data + "\"");             
         });
         child.stderr.on('data', function(data) {
             console.log(data);
@@ -41,7 +38,7 @@ function initTelegram(isStdoutOn) {
     child.stdin.write("contact_list\n"); //без него будет "can not parse arg #1"  
     return child; 
 }
-
+/*
 function validateAndParse(stringToParse) {
     console.log("result: " + stringToParse);
     if (stringToParse == "") return "";
@@ -57,4 +54,4 @@ function validateAndParse(stringToParse) {
     return result;
 
 }
-
+*/
