6cb26abf837595412097e101188528c3194f5d34
Alexandr <new_bazzz@mail.ru>
2015-06-24 13:06:44 +0400

    chenges

--- telenode.js
+++ telenode.js
@@ -10,8 +10,8 @@ var resurs;
 var server = http.createServer(function(req, res) {
         var pathname = url.parse(req.url).pathname.substring(1);
         resurs = res;
-        pathname = decode(pathname);         
-//        pathname =  decodeURIComponent(pathname);
+//        pathname = decode(pathname);         
+        pathname =  decodeURIComponent(pathname);
         var command = cmd + '"' + pathname + '"';
         console.log("command: " + command + "\n");
         exec(command, puts);
