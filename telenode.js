80eb156e41217ee34dd92f8ecfa3e949fe8b5ffc
Alexandr <new_bazzz@mail.ru>
2015-06-24 12:09:13 +0400

    chenges

--- telenode.js
+++ telenode.js
@@ -2,40 +2,22 @@ var http = require("http");
 var url = require("url");
 var exec = require("child_process").exec;
 var child = initTelegram(false);
+var cmd = "../tg/bin/telegram-cli -k ../tg/tg-server.pub -W -e ";
 
 var server = http.createServer(function(req, res) {
         var pathname = url.parse(req.url).pathname.substring(1);
 
         pathname = decode(pathname);
         
-        var command = pathname + "\n";
+        var command = pathname;
         console.log("command: " + command);
-        child.stdin.write(command);
+        child = exec(cmd + '"' + command + '"');
         res.writeHead(200);
         res.end("OK");
     }
 );
 
 server.listen(7777);
-
-function initTelegram(isStdoutOn) {
-    child = exec("../tg/bin/telegram-cli -k ../tg/tg-server.pub"); // run telegram client
-    child.stdin.setEncoding = 'utf-8';
-    if (isStdoutOn) {
-        child.stdout.on('data', function(data) {
-            if (data == "> ") {return;}             
-            console.log("DATA \"" + data + "\"");             
-        });
-        child.stderr.on('data', function(data) {
-            console.log(data);
-        });
-    }
-    child.on('close', function(code) {
-                console.log('closing code: ' + code);
-    });
-    child.stdin.write("contact_list\n"); //без него будет "can not parse arg #1"  
-    return child; 
-}
 
 function decode (what) {
     var result = "";
